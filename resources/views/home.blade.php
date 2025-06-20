f800d726fa6a56f52c9ccb40f35a3b27a4fa416d
Am <noramirul.nordin@swmsb.com>
2025-06-20 23:36:13 +0800

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,6 @@
             </div>
         @endif
         <div class="row">
-
-
             <div class="col-xl-12">
                 <div class="row">
                     <div class="col-12 mb-4 d-flex align-items-center justify-content-between">
